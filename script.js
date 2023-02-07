2f5014b99509807811558f1a113c1d5e6c464bc7
sawmon <sawmonhan@acedatasystems.com>
2023-02-07 10:45:14 +0100

    update the changes

--- script.js
+++ script.js
@@ -1,45 +1,36 @@
 const brand = document.querySelector('.brand');
-const checkbox = document.querySelector('.toggler');
+const checkbox = document.querySelector('#toggle');
 const intro = document.querySelector('.intro');
 const hamburger = document.querySelector('.hamburger');
 const works = document.querySelector('.works');
 const aboutMyself = document.querySelector('.about_myself');
-const navlinks = document.querySelectorAll('.nav-links');
-checkbox.addEventListener('click', () => {
-  brand.classList.toggle('blur');
-  intro.classList.toggle('blur');
-  works.classList.toggle('blur');
-  aboutMyself.classList.toggle('blur');
+function remove() {
+  document.getElementById('toggle').checked = false;
+  brand.classList.remove('blur');
+  intro.classList.remove('blur');
+  works.classList.remove('blur');
+  aboutMyself.classList.remove('blur');
+  hamburger.style.position = 'absolute';
+}
 
-  function removeOverlay() {
-    document.getElementsByClassName('toggler').checked = false;
-    brand.classList.remove('blur');
-    intro.classList.remove('blur');
-    works.classList.remove('blur');
-    aboutMyself.classList.remove('blur');
-    hamburger.style.position = 'fixed';
-  }
-
+function toggle() {
   if (checkbox.checked) {
     hamburger.style.position = 'fixed';
-    brand.classList.remove('blur');
-    intro.classList.remove('blur');
-    works.classList.remove('blur');
-    aboutMyself.classList.remove('blur');
+    brand.classList.toggle('blur');
+    intro.classList.toggle('blur');
+    works.classList.toggle('blur');
+    aboutMyself.classList.toggle('blur');
   } else {
     hamburger.style.position = 'absolute';
+    remove();
   }
-});
+}
 
 let w = 0;
 window.onresize = () => {
   w = window.innerWidth;
   if (w > 768) {
-    document.getElementsByClassName('toggler').checked = false;
-    brand.classList.remove('blur');
-    intro.classList.remove('blur');
-    works.classList.remove('blur');
-    aboutMyself.classList.remove('blur');
+    toggle();
   }
 };
 
