09b0b05880b97a63d38cb2dc3f11990766851528
sawmon <sawmonhan@acedatasystems.com>
2023-02-07 05:43:13 +0100

    stylelint errors

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
-const brand = document.querySelector(".brand");
-const checkbox = document.querySelector(".toggler");
-const intro = document.querySelector(".intro");
-const hamburger = document.querySelector(".hamburger");
-const works = document.querySelector(".works");
-const aboutMyself = document.querySelector(".about_myself");
-checkbox.addEventListener("click", function (e) {
-  brand.classList.toggle("blur");
-  intro.classList.toggle("blur");
-  works.classList.toggle("blur");
-  aboutMyself.classList.toggle("blur");
+const brand = document.querySelector('.brand');
+const checkbox = document.querySelector('.toggler');
+const intro = document.querySelector('.intro');
+const hamburger = document.querySelector('.hamburger');
+const works = document.querySelector('.works');
+const aboutMyself = document.querySelector('.about_myself');
+const navlinks = document.querySelectorAll('.nav-links');
+checkbox.addEventListener('click', function toggleBlur() {
+  brand.classList.toggle('blur');
+  intro.classList.toggle('blur');
+  works.classList.toggle('blur');
+  aboutMyself.classList.toggle('blur');
 
-  $(".nav-links").click(function () {
-    $(".toggler").prop("checked", false);
-    brand.classList.remove("blur");
-    intro.classList.remove("blur");
-    works.classList.remove("blur");
-    aboutMyself.classList.remove("blur");
-    hamburger.style.position = "absolute";
+  navlinks.addEventListener('click', function removeBlur() {
+    document.getElementsByClassName('toggler').checked = false;
+    brand.classList.remove('blur');
+    intro.classList.remove('blur');
+    works.classList.remove('blur');
+    aboutMyself.classList.remove('blur');
+    hamburger.style.position = 'fixed';
   });
 
   if (checkbox.checked) {
-    hamburger.style.position = "fixed";
+    hamburger.style.position = 'fixed';
+    brand.classList.remove('blur');
+    intro.classList.remove('blur');
+    works.classList.remove('blur');
+    aboutMyself.classList.remove('blur');
   } else {
-    hamburger.style.position = "absolute";
+    hamburger.style.position = 'absolute';
   }
 });
 
@@ -30,11 +35,10 @@ let w = 0;
 window.onresize = function () {
   w = window.innerWidth;
   if (w > 768) {
-    $(".toggler").prop("checked", false);
-
-    brand.classList.remove("blur");
-    intro.classList.remove("blur");
-    works.classList.remove("blur");
-    aboutMyself.classList.remove("blur");
+    document.getElementsByClassName('toggler').checked = false;
+    brand.classList.remove('blur');
+    intro.classList.remove('blur');
+    works.classList.remove('blur');
+    aboutMyself.classList.remove('blur');
   }
 };
